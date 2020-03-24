d6b46c86a4d9f97de06d4dfc92df981163ee108b
Hugo Sobral <hugosobral10@gmail.com>
2020-03-24 17:58:26 -0300

    Add ongs info into incidents index method

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -21,9 +21,16 @@ module.exports = {
     const [count] = await connection("incidents").count();
 
     const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
       .limit(5)
       .offset((page - 1) * 5)
-      .select("*");
+      .select([
+        "incidents.*",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
 
     res.header("X-Total-Count", count["count(*)"]);
     res.header("X-Total-Pages", parseInt(count["count(*)"] / 5));
