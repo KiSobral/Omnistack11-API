722b64269d6c1739a19e5f62ba9f9bae25eb5c09
Hugo Sobral <hugosobral10@gmail.com>
2020-03-24 17:34:27 -0300

    Create login and profile methods

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
@@ -12,5 +15,11 @@ routes.post("/ongs", OngController.create);
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
 routes.delete("/incidents/:id", IncidentController.delete);
+
+// Ongs profile route
+routes.get("/profile", ProfileController.index);
+
+// Login routes
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
