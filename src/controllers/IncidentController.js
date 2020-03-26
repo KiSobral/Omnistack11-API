8e3c86a08162a93335cea57894ade328ed3fa886
Hugo Sobral <hugosobral10@gmail.com>
2020-03-26 20:20:58 -0300

    Update incidents index method

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -26,6 +26,7 @@ module.exports = {
       .offset((page - 1) * 5)
       .select([
         "incidents.*",
+        "ongs.name",
         "ongs.email",
         "ongs.whatsapp",
         "ongs.city",
